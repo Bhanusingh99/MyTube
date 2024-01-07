e24f933886d9e77da3faf02ccaf10762e23c8daf
Bhanu <bhanusngh12345@gmail.com>
2024-01-07 13:12:02 +0530

    add user and commit model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from 'dotenv'
 import connectionDb from '../src/db/index.js'
+import app from './app.js'
+
 
 dotenv.config({
     path:'./env'
 })
 
-connectionDb();
+const PORT = process.env.PORT || 8000;
+connectionDb()
+.then(()=>{
+  app.listen(PORT,() => {
+    console.log("app is listining on port no:3000");
+  })
+})
+.catch((error)=>{
+   console.log(error);
+})
+
+app.get("/",(req,res) => {
+    res.send("<h1>This is your first Express project</h1>")
+})
---
+++ src/models/user.js
@@ -0,0 +1,90 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userShema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, // comming from cloudniry
+      required: true,
+    },
+    coverImage: {
+      type: String, // coming from cloudniry
+    },
+    Password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistor: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userShema.pre("save", async function (next) {
+  if (!this.isModified("Password")) return next();
+
+  this.Password = bcrypt.hash(this.Password, 10);
+  next();
+});
+
+userShema.methods.isPasswordCorrect = async function (Password) {
+  return await bcrypt.compare(Password, this.Password);
+};
+
+userShema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      fullname: this.fullname,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userShema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+          _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+      );
+}
+
+export const User = mongoose.model("User", userShema);
