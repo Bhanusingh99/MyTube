7e9a8b619e086c072ab9acb448b517bef79d6d54
Bhanu <bhanusngh12345@gmail.com>
2024-01-09 10:01:55 +0530

    config setup

--- src/app.js
+++ src/app.js
@@ -12,4 +12,9 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+
+import { router } from './routes/users.routes.js';
+app.use("/api/v1/users",router)
+
+
 export default app;
